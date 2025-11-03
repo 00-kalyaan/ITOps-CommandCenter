ff49480b2653b0cbd4c1c372bffc6b2b7b9c0369
00-kalyaan <83536199+00-kalyaan@users.noreply.github.com>
2025-11-03 02:38:32 -0500

    Update scheduler.py

--- src/app/scheduler.py
+++ src/app/scheduler.py
@@ -3,25 +3,30 @@ from sqlmodel import Session, select
 from .models import Asset, Metric
 from .collectors.sys_metrics import collect_local_metrics
 from .collectors.net_probe import ping_host
-
+from .rules import evaluate_latest_metric
 
 scheduler = BackgroundScheduler()
 
 
 def job_collect_metrics(engine):
-with Session(engine) as s:
-for asset in s.exec(select(Asset)).all():
-m = collect_local_metrics(asset) # for demo, treat all as local
-m.ping_ms = ping_host(asset.ip)
-s.add(m)
-s.commit()
-
-
-_defers = {}
+    # This function collects metrics for all assets and applies threshold rules
+    with Session(engine) as s:
+        assets = s.exec(select(Asset)).all()
+        for asset in assets:
+            # Collect system metrics (CPU, RAM, Disk)
+            m = collect_local_metrics(asset)
+            # Measure latency / ping
+            m.ping_ms = ping_host(asset.ip)
+            # Save metric and evaluate against rules
+            s.add(m)
+            s.commit()
+            evaluate_latest_metric(s, m)
+        s.commit()
 
 
 def start_scheduler(engine):
-if scheduler.running:
-return
-scheduler.add_job(job_collect_metrics, "interval", minutes=1, args=[engine], id="collect")
-scheduler.start()
+    """Start APScheduler background job"""
+    if scheduler.running:
+        return
+    scheduler.add_job(job_collect_metrics, "interval", minutes=1, args=[engine], id="collect")
+    scheduler.start()
