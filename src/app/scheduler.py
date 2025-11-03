6f184f8fbc322d01e77119b3b0bb9afea72f40a7
00-kalyaan <83536199+00-kalyaan@users.noreply.github.com>
2025-11-03 02:13:53 -0500

    Update scheduler.py

--- src/app/scheduler.py
+++ src/app/scheduler.py
@@ -1,1 +1,27 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from sqlmodel import Session, select
+from .models import Asset, Metric
+from .collectors.sys_metrics import collect_local_metrics
+from .collectors.net_probe import ping_host
 
+
+scheduler = BackgroundScheduler()
+
+
+def job_collect_metrics(engine):
+with Session(engine) as s:
+for asset in s.exec(select(Asset)).all():
+m = collect_local_metrics(asset) # for demo, treat all as local
+m.ping_ms = ping_host(asset.ip)
+s.add(m)
+s.commit()
+
+
+_defers = {}
+
+
+def start_scheduler(engine):
+if scheduler.running:
+return
+scheduler.add_job(job_collect_metrics, "interval", minutes=1, args=[engine], id="collect")
+scheduler.start()
